e3cc85d95335a48d45d874168db659c372d7ee30
Peter Graham <peter@1000memories.com>
2012-09-09 12:51:12 -0700

    Add inaban

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-document.head.innerHTML += "<style>*{margin:0;padding:0}body{font:100% monospace;text-align:center;padding-top:50px;}h1,h2{font-weight:400}h2{padding-top:10px;font-size:100%}</style>";
+document.head.innerHTML += "<style>*{margin:0;padding:0}body{font:100% monospace;text-align:center}img{line-height:0%;position:absolute;bottom:0;left:50%;margin-left:-227px}h1{font-weight:400;padding:10px;text-align:left;font-size:100%}</style>";
 
 randomRange = function(from, to) {
   return Math.floor(Math.random() * (to + 1)) + from;
@@ -8,18 +8,27 @@ pickRandom = function(arr) {
   return arr[randomRange(0, arr.length - 1)];
 };
 
-window.onload = function() {
-  document.body.innerHTML = "<h1>moe</h1><h2></h2>";
-  var h2 = document.getElementsByTagName("h2")[0];
-  var newTitle = "";
-  var separators = ["#", "%", "^", "*", "=", "?", "/", "+", "_", "-", "~", "!", ":", "$", "."];
+randomCap = function(str) {
+  var chars = str.split("");
+  var newString = "";
+  for(var i = 0; i < chars.length; i++) {
+    newString += randomRange(0, 1) == 0 ? chars[i].toUpperCase() : chars[i].toLowerCase();
+  }
+  return newString;
+};
 
-  for(var i = 0; i < 5; i++) newTitle += "moe"+pickRandom(separators);
-  h2.innerHTML = newTitle;
+window.onload = function() {
+  document.body.innerHTML = "<img src='assets/inaban.png?' alt='moe'><h1>moe</h1>";
+  var h1 = document.getElementsByTagName("h1")[0];
+  var newTitle = "";
+  var separators = "#%^*=?/+_-~!:$.".split("");
+
+  for(var i = 0; i < 5; i++) newTitle += randomCap("moe")+pickRandom(separators);
+  h1.innerHTML = newTitle;
   var firstChar;
   setInterval(function(){
-    firstChar = h2.innerHTML.slice(0, 1);
+    firstChar = h1.innerHTML.slice(0, 1);
     if(separators.indexOf(firstChar) >= 0) firstChar = pickRandom(separators);
-    h2.innerHTML = h2.innerHTML.slice(1) + firstChar;
+    h1.innerHTML = h1.innerHTML.slice(1) + firstChar;
   }, 110);
 };
